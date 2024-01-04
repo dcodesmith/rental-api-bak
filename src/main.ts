049aeaa064e0fa56ca1d5898f9214098cfe8f371
Afees Kolawole <dcodesmith@gmail.com>
2024-01-04 18:02:44 +0100

    remove `api` global prefix

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3001;
   await app.listen(port);
   Logger.log(
